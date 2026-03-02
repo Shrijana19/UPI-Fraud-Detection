c6484e32ad36a9cc27bc8a8220495764d450c677
Shrijana Bhujel <sollbhujel19@gmail.com>
2026-03-02 23:11:52 +0530

    Update touch ml_engine.py

--- touch ml_engine.py
+++ touch ml_engine.py
@@ -1,1 +1,10 @@
-
+class MLEngine:
+    def predict_probability(self, amount):
+        if amount > 25000:
+            return 0.8
+        elif amount > 15000:
+            return 0.6
+        elif amount > 8000:
+            return 0.4
+        else:
+            return 0.1
