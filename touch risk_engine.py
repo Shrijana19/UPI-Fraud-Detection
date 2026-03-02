b889b99a2e7de266cc5e6311610e0a7d44e02144
Shrijana Bhujel <sollbhujel19@gmail.com>
2026-03-02 23:12:22 +0530

    Update touch risk_engine.py

--- touch risk_engine.py
+++ touch risk_engine.py
@@ -1,1 +1,16 @@
+class RiskEngine:
+    def aggregate_risk(self, ml_prob, graph_risk, behavior_risk):
+        final_score = (
+            0.5 * ml_prob +
+            0.3 * graph_risk +
+            0.2 * behavior_risk
+        )
+        return min(final_score, 1.0)
 
+    def make_decision(self, risk_score):
+        if risk_score < 0.4:
+            return "APPROVED"
+        elif risk_score < 0.7:
+            return "STEP-UP AUTH"
+        else:
+            return "BLOCKED"
